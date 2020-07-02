d4b24190b042aff1ca27717652968a9483dff628
Lucas Machado <machadolucasvp@gmail.com>
2020-07-02 01:35:02 -0300

    feat: ༼ つ ◕_◕ ༽つ initial commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import Koa from 'koa';
+import json from 'koa-json';
+import logger from 'koa-logger';
+
+const app = new Koa();
+
+app.use(json());
+app.use(logger());
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.body = {
+    ...ctx.body,
+    requestTime: `${ms}ms`,
+  };
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(async (ctx) => {
+  ctx.body = { message: 'hello world!' };
+});
+
+app.listen(3333, () => {
+  console.log('Running in port 3333');
+});
